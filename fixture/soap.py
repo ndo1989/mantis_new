91259704c948d9e67f4e15c81538904bc3af2aae
dnevaykin <40721547+dnevaykin@users.noreply.github.com>
2021-02-19 11:21:45 +0300

    Задание №26, попытка №2. Реализованы проверки списков проектов через SOAP. В файле soap.py, а взят за "основу адреса" из конфигурационного файла

--- fixture/soap.py
+++ fixture/soap.py
@@ -9,7 +9,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-2.24.4/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.baseUrl + "/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -17,7 +17,7 @@ class SoapHelper:
             return False
 
     def get_projects_list(self, username, password):
-        client = Client("http://localhost/mantisbt-2.24.4/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.baseUrl + "/api/soap/mantisconnect.php?wsdl")
 
         def convert(project):
             return Project(id=str(project.id), name=project.name, description=project.description)
